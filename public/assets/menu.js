ba1144781aef0e799229ce2fe80370f282134806
DanielTisue <d.tisue@gmail.com>
2020-02-23 09:25:15 -0600

    Redid landing page organization. Remobed menu svgs and used text styling and positioning. Accomodated for mobile styling in both menu.css, and main.css. Re-did hamburger menu and used js. Added menu.js file and assets folder. Added instagram and facebook svg icons. Adjusted containers for landing.

--- public/assets/menu.js
+++ public/assets/menu.js
@@ -1,9 +1,35 @@
 
+const hamburger = document.querySelector('.hamburger');
+const navlinks = document.querySelector('.ul-alignment');
+const topper = document.getElementById('rotate-top');
+const bottom = document.getElementById('rotate-bottom');
+const middle = document.getElementById('remove-center');
 
-function hideMenu() {
-   
-  
- if (document.querySelector('.toggler').checked = true) {
-   document.querySelector('.toggler').checked = false;
+hamburger.addEventListener('click', () => {
+console.log('clicked');
+navlinks.classList.toggle('open');
+topper.classList.toggle('rotate-top');
+bottom.classList.toggle('rotate-bottom');
+middle.classList.toggle('remove-center');
+});
+
+
+function hideMenu() {  
+ if (navlinks.classList.contains('open')) {
+   navlinks.classList.remove('open');
+   topper.classList.remove("rotate-top");
+   bottom.classList.remove("rotate-bottom");
+   middle.classList.remove("remove-center");
  }
-  }
+}
+
+// const x = window.matchMedia("(max-width: 480px");
+// const linkTag = document.querySelector('.nav-brand-link');
+// const brandHref = document.querySelector('[href="/"]');
+
+// function brandHide(x) {
+//   if (x.matches) {
+//     brandHref = '';
+//   }
+// }
+
