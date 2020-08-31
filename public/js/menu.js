d1a6be56f96956ccb6774daf03d8a6d1bc15b1dd
DanielTisue <d.tisue@gmail.com>
2020-08-30 20:53:50 -0500

    Added on scroll animations using js observer api method.

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,7 +1,6 @@
 
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
-// const navLinks = document.querySelectorAll('.nav-item-menu')
 const topper = document.getElementById('rotate-top');
 const bottom = document.getElementById('rotate-bottom');
 const middle = document.getElementById('remove-center');
@@ -10,7 +9,6 @@ const middle = document.getElementById('remove-center');
 hamburger.addEventListener('click', () => {
 console.log('clicked');
 navMenu.classList.toggle('active');
-// navLinks.classList.toggle('link-active');
 topper.classList.toggle('rotate-top');
 bottom.classList.toggle('rotate-bottom');
 middle.classList.toggle('remove-center');
@@ -24,8 +22,26 @@ function hideMenu() {
    bottom.classList.remove("rotate-bottom");
    middle.classList.remove("remove-center");
  }
-
 }
 
+const elements = document.querySelectorAll('.anim');
+
+observer = new IntersectionObserver((entries) => {
+entries.forEach(entry => {
+
+  if(entry.intersectionRatio > 0) {
+      entry.target.style.animation = `slideInFromRight 2s ${entry.target.dataset.delay} forwards ease-out`;
+    } else {
+      entry.target.style.animation = 'none';
+    }
+
+  });
+});
+
+
+ 
+elements.forEach(element => {
+observer.observe(element);
+});
 
 
